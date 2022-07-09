60ebf2dbd3c9048f54e6c7fb6011b189d302b109
AsterisMono <cmiki@amono.me>
2022-07-09 20:04:52 +0800

    Fix title strip again Add some more keywords

--- influnc_plus/util/utils.py
+++ influnc_plus/util/utils.py
@@ -4,8 +4,8 @@ rex = re.compile(r'\W+')
 
 
 def str_collapse(string: str) -> str:
-    return rex.sub(' ', string)
+    return rex.sub(' ', string).strip()
 
 
 if __name__ == '__main__':
-    print(str_collapse("测试\n            测试       \n"))
+    print(str_collapse("   测 试\n            测试       \n  "))
