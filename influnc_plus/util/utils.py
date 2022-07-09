2126a899d94d197804be87f95be282ff5f95d0fe
AsterisMono <cmiki@amono.me>
2022-07-09 21:56:55 +0800

    Update keyword list Fix dropping semicolons in str_collapse

--- influnc_plus/util/utils.py
+++ influnc_plus/util/utils.py
@@ -1,7 +1,7 @@
 import logging
 import re
 
-rex = re.compile(r'\W+')
+rex = re.compile(r'\s{2,}')
 
 
 def str_collapse(string: str) -> str:
@@ -14,3 +14,8 @@ def str_collapse(string: str) -> str:
 
 if __name__ == '__main__':
     print(str_collapse("   测 试\n            测试       \n  "))
+    print(str_collapse(" Felix's \n cat"+"""
+    
+    
+    hel     lo?
+    """))
