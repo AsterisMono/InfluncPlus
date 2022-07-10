35462b4d629d7b6fe8b7b6d623e820810611cb76
AsterisMono <cmiki@amono.me>
2022-07-10 08:28:28 +0800

    Limit concurrent requests to avoid deadlock Update keyword list

--- influnc_plus/settings.py
+++ influnc_plus/settings.py
@@ -19,7 +19,7 @@ NEWSPIDER_MODULE = 'influnc_plus.spiders'
 ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS = 128
+CONCURRENT_REQUESTS = 32
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
